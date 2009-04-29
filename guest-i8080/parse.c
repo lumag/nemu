801842d92eed42d91c03ac6f314cd2b10b310ff8
Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>
2009-04-29 17:32:58 +0400

    Include config.h if it's available
    Signed-off-by: Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>

--- guest-i8080/parse.c
+++ guest-i8080/parse.c
@@ -17,6 +17,11 @@
  *  with this program; if not, write to the Free Software Foundation, Inc.,
  *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
 #include <stdio.h>
 
 #include "i8080.h"
--- guest-i8080/test.c
+++ guest-i8080/test.c
@@ -17,6 +17,11 @@
  *  with this program; if not, write to the Free Software Foundation, Inc.,
  *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/mman.h>
